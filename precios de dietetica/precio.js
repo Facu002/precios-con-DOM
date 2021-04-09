cb36ada2973c9c2dfc4ad4af96407ea1aabd1c50
Facu002 <78241468+Facu002@users.noreply.github.com>
2021-04-09 11:11:16 -0300

    Add files via upload

---
+++ precios de dietetica/precio.js
@@ -0,0 +1,6 @@
+function chia(){ document.getElementById("precio").innerHTML="la chia sale 30 los 100"
+}
+function avena(){ document.getElementById("precio").innerHTML="la avena sale 130 el kilo"
+}
+function mani(){ document.getElementById("precio").innerHTML="el mani sale 25 los 100"
+}
